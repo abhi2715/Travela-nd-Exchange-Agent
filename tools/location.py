0ba1cdeed5dab8657270bfca9535ff8decb43667
abhi2715 <155663986+abhi2715@users.noreply.github.com>
2026-02-13 11:11:28 +0530

    Update location.py

--- tools/location.py
+++ tools/location.py
@@ -1,1 +1,19 @@
+import requests
+
+def get_country_location(country: str):
+    try:
+        url = f"https://restcountries.com/v3.1/name/{country}"
+        res = requests.get(url, timeout=10).json()[0]
+
+        lat, lon = res["latlng"]
+
+        return {
+            "country": res["name"]["common"],
+            "latitude": lat,
+            "longitude": lon,
+            "region": res["region"],
+            "subregion": res.get("subregion", "N/A"),
+        }
+    except Exception:
+        return {"error": "Location data not available"}
 
